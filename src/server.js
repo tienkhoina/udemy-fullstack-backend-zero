fa8a17b3326d8e007f0ff8bacf3335ddcb291069
tienkhoina <tienkhoina@gmail.com>
2024-09-29 10:43:32 +0700

    update

--- src/server.js
+++ src/server.js
@@ -10,8 +10,11 @@ const hostname= process.env.HOST_NAME
 
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname,'public')))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! leu leu')
 })
 app.get('/test', (req, res) => {
     res.send('test')
