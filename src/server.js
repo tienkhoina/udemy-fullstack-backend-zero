e6639808258649139b1927e110dbc67a2f5e96b0
tienkhoina <tienkhoina@gmail.com>
2024-09-29 10:06:33 +0700

    update

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path= require('path');// commonjs
+require('dotenv').config()
+const app = express()
+const port =  process.env.PORT
+const hostname= process.env.HOST_NAME
+
+
+
+
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/test', (req, res) => {
+    res.send('test')
+  })
+  app.get('/tienkhoi', (req, res) => {
+   res.render('sample.ejs')
+  })
+app.listen(port, hostname, () => {
+  console.log(`Example app listening on port ${port}`)
+})
