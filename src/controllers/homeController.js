96fc630620d42b0b9da1abc87d991945dc5b5ee5
tienkhoina <tienkhoina@gmail.com>
2024-10-03 21:28:57 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,21 @@
+const { json } = require('express');
+const connection = require('D:\\backendtest\\src\\config\\database.js')
+
 const getHomepage = (req,res)=>{
-    res.send('Hello World! leu leu')
+
+    let users=[];
+
+    connection.query(
+        'select * from Users ',
+        function (err, results, fields) {
+          users = results
+          console.log(">>>>result homepage =",results); // results contains rows returned by server
+          console.log(">>> fields=",fields); // fields contains extra meta data about results, if available
+        }
+      );
+    //console.log("check users", users)  
+
+    res.send(JSON.stringify(users))
 }
 
 const getTest= (req,res)=>{
