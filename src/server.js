8cd56f9c631b7348ba887a3993a5418a633efa40
tienkhoina <tienkhoina@gmail.com>
2024-10-03 20:38:08 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path= require('path');// commonjs
 require('dotenv').config()
-
+const mysql = require('mysql2');
 const configViewEngine = require("./config/viewEngine")
 const webRoutes = require("./routes/web")
 
@@ -15,6 +15,26 @@ configViewEngine(app)
 app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/',webRoutes)
+// Get the client
+
+
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password:'Letienkhoi1710',
+  database: 'backendtest',
+});
+
+connection.query(
+  'select * from Users ',
+  function (err, results, fields) {
+    console.log(">>>>result=",results); // results contains rows returned by server
+    console.log(">>> fields=",fields); // fields contains extra meta data about results, if available
+  }
+);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
